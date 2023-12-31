8efdd8efd4f9f92eb6169661e1ebcf55599a0dfb
Daniel Zych <danjzych@gmail.com>
2023-12-31 16:36:36 -0600

    Forgot to clean up one of the components from previous commit, deleted unused code from Projects.svelte

--- src/components/sections/Projects.svelte
+++ src/components/sections/Projects.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
 	import type { Project } from '../../types/interfaces';
 	import ProjectItem from '../ProjectItem.svelte';
 	import PROJECTS from '$lib/content/projects.json';
 	import { scrollToSection } from '$lib/utils';
-	import useIntersectionObserver from '$lib/useIntersectionObserver';
 
 	const projects: Project[] = PROJECTS;
 </script>
@@ -12,8 +10,6 @@
 <section
 	id="projects"
 	class="flex min-h-screen min-w-full flex-col items-center gap-6"
-	use:useIntersectionObserver
-	on:enterView={() => (inView = true)}
 >
 	<h2
 		class="mt-32 flex w-3/5 items-center justify-between pb-6 text-3xl font-semibold"
