5e07278ecc9adc284dfcf9630038cebd5b0feaf3
Daniel Zych <danjzych@gmail.com>
2024-01-01 20:39:18 -0600

    Added static adapter for deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import { AdapterOptions } from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,9 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: null,
+			fallback: undefined,
+			precompress: false,
+			strict: true,
 		}),
 	},
 };
